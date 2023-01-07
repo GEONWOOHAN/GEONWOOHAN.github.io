a9588aac08c435bebdec2c6db019bc01e06bc1ca
GEONWOOHAN <gunwoo9643@naver.com>
2023-01-08 02:43:42 +0900

    +

--- js/weather.js
+++ js/weather.js
@@ -1,19 +1,23 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
 const API_KEY = "20605362e67ac1aaebcc9b669dcf3e5d";
 
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
+    const lng = position.coords.longitude;
     console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url).then(response => response.json()).then(data => {
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
-        city.innerHTML = date.name;
-        weather.innerHTML = `${data.weather[0].main} / ${data.main.temp}`;
-    });
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
 }
-function onGeoError() {
 
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
 }
 
 
