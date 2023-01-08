120207122d2de0f7dbc399c32fa048f98a728827
GEONWOOHAN <gunwoo9643@naver.com>
2023-01-08 18:31:09 +0900

    +

--- js/todo.js
+++ js/todo.js
@@ -23,7 +23,7 @@ function paintToDo(newTodo) {
     const span = document.createElement("span");
     span.innerText = newTodo.text;
     const button = document.createElement("button");
-    button.innerText = "🆗✔️☑️✅⭕";
+    button.innerText = "✔️";
     button.addEventListener("click", deleteToDo);
     li.appendChild(span);
     li.appendChild(button);
